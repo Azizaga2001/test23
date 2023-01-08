55b0103bc924d08d552693cde5a81d03c635d0bf
Avatar-shop <110927462+Azizaga2001@users.noreply.github.com>
2023-01-08 18:14:10 +0400

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,18 @@
+//1)
+//в чем + стрелочных функций?
+
+//ona udobney i koroche
+
+//2)
+//превратите функцию ниже в стрелочную
+let newUser = (name, age) => {
+    if (age > 18) {
+        console.log(`${name} ты стар`);
+    } else {
+        console.log(`${name} ты не дорос`);
+    }
+}
+
+//3)
+//сделай скрипт ниже короче
+let call = (a, b) => a + b;
